11b366a10989f972a8e3bd4245705366d4fb412c
saim368 <saimharoon62@gmail.com>
2020-11-06 11:46:54 -0500

    generate static blog

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <section class="hero is-primary has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            <nuxt-link to="/">Nuxt Blog</nuxt-link>
+          </h1>
+          <h2 class="subtitle">
+            Your all round tech blog
+          </h2>
+        </div>
+      </div>
+    </section> 
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+</style>
+
---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+const client = require('./plugins/contentful')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-blog',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      {
+        rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css'
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  plugins: ['~/plugins/contentful'],
+  modules: ['@nuxtjs/dotenv', '@nuxtjs/markdownit'],
+  markdownit: {
+    injected: true
+  },
+  generate: {
+    routes () {
+      return client.getEntries({'content_type': 'post'})
+      .then(entries => {
+        return entries.items.map(entry => {
+          return {
+            route: entry.fields.slug,
+            payload: entry
+          }
+        })
+      })
+    }
+  }
+}
+
